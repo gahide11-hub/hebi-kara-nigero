e548abfca762be0589725cc454c11a7db3eeffa6
gahide11-hub <g.a.hide11@gmail.com>
2026-07-20 16:08:32 +0900

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,64 @@
+const map = [
+"##########",
+"#P.......#",
+"#..###...#",
+"#......#.#",
+"#..#....E#",
+"#........#",
+"##########"
+];
+
+
+const game = document.getElementById("game");
+
+
+function draw(){
+
+    game.innerHTML = "";
+
+    for(let y = 0; y < map.length; y++){
+
+        for(let x = 0; x < map[y].length; x++){
+
+            const cell = document.createElement("div");
+
+            cell.className = "cell";
+
+
+            if(map[y][x] === "#"){
+
+                cell.textContent = "🌳";
+
+            }
+
+            else if(map[y][x] === "P"){
+
+                cell.textContent = "🙂";
+
+            }
+
+            else if(map[y][x] === "E"){
+
+                cell.textContent = "🌿";
+
+            }
+
+
+            game.appendChild(cell);
+
+        }
+
+    }
+
+}
+
+
 function startGame(){
 
     document.getElementById("titleScreen").style.display="none";
 
     document.getElementById("gameScreen").style.display="block";
+
+    draw();
 
 }
