d7e528b69e8661b942b74ca9432b52e8b39032b2
gahide11-hub <g.a.hide11@gmail.com>
2026-07-20 16:22:36 +0900

    Refactor game drawing logic and startGame function

--- game.js
+++ game.js
@@ -1,56 +1,17 @@
 const map = [
+
 "##########",
 "#P.......#",
 "#..###...#",
-"#......#.#",
-"#..#....E#",
+"#........#",
+"#..###..E#",
 "#........#",
 "##########"
+
 ];
 
 
 const game = document.getElementById("game");
-
-
-function draw(){
-
-    game.innerHTML = "";
-
-    for(let y = 0; y < map.length; y++){
-
-        for(let x = 0; x < map[y].length; x++){
-
-            const cell = document.createElement("div");
-
-            cell.className = "cell";
-
-
-            if(map[y][x] === "#"){
-
-                cell.textContent = "🌳";
-
-            }
-
-            else if(map[y][x] === "P"){
-
-                cell.textContent = "🙂";
-
-            }
-
-            else if(map[y][x] === "E"){
-
-                cell.textContent = "🌿";
-
-            }
-
-
-            game.appendChild(cell);
-
-        }
-
-    }
-
-}
 
 
 function startGame(){
@@ -60,5 +21,53 @@ function startGame(){
     document.getElementById("gameScreen").style.display="block";
 
     draw();
+
+}
+
+
+
+function draw(){
+
+    game.innerHTML="";
+
+
+    for(let y=0; y<map.length; y++){
+
+
+        for(let x=0; x<map[y].length; x++){
+
+
+            const cell=document.createElement("div");
+
+            cell.className="cell";
+
+
+            if(map[y][x]=="#"){
+
+                cell.textContent="🌳";
+
+            }
+
+
+            else if(map[y][x]=="P"){
+
+                cell.textContent="🙂";
+
+            }
+
+
+            else if(map[y][x]=="E"){
+
+                cell.textContent="🌿";
+
+            }
+
+
+            game.appendChild(cell);
+
+
+        }
+
+    }
 
 }
